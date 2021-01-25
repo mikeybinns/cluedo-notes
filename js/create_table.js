6dfb03ee3eb31ee9eade644bd6c6d08578d6b24c
Mikey Binns <38146638+mikeybinnswebdesign@users.noreply.github.com>
2021-01-25 23:08:53 +0000

    Styling of the table

--- js/create_table.js
+++ js/create_table.js
@@ -15,6 +15,7 @@ export function catch_form_submit() {
 				5: document.getElementById('player_6').value,
 			},
 		};
+		add_colgroup_data(player_data['player count']);
 		add_player_headers(player_data);
 		add_separator_row('Suspects');
 		add_suspect_rows(player_data['player count']);
@@ -24,6 +25,7 @@ export function catch_form_submit() {
 		add_location_rows(player_data['player count']);
 		add_event_listeners_to_buttons();
 		document.getElementById('notes_table').classList.remove('hide');
+		set_player_name_height(player_data['player count']);
 		document.getElementById('start_screen').classList.add('hide');
 	});
 }
@@ -38,9 +40,25 @@ function create_button_row() {
 	return row;
 }
 
+function add_colgroup_data(player_count) {
+	const colgroup = document.createElement('colgroup'),
+		header_col = document.createElement('col');
+	header_col.span = 1;
+	header_col.style.width = 'auto';
+	colgroup.append(header_col);
+	for (let $i = 0; $i < player_count; $i++) {
+		const col = document.createElement('col');
+		col.span = 1;
+		col.style.width = '2rem';
+		colgroup.append(col);
+	}
+	const table = document.getElementById('notes_table');
+	table.prepend(colgroup);
+}
+
 function add_player_headers(player_data) {
-	const players_row = document.getElementById('players_row');
-	const header = document.createElement('th');
+	const players_row = document.getElementById('players_row'),
+		header = document.createElement('th');
 	header.scope = 'col';
 	header.classList.add('player_heading');
 	const player_label = header.cloneNode();
@@ -49,8 +67,11 @@ function add_player_headers(player_data) {
 	players_row.append(player_label);
 
 	for (let $i = 0; $i < player_data['player count']; $i++) {
-		let player_header = header.cloneNode();
-		player_header.innerHTML = player_data.players[$i];
+		let player_header = header.cloneNode(),
+			span = document.createElement('span');
+		span.id = `player_name_${$i + 1}`;
+		span.innerHTML = player_data.players[$i];
+		player_header.append(span);
 		players_row.append(player_header);
 	}
 }
@@ -59,9 +80,7 @@ function add_suspect_rows(player_count) {
 	const table_body = document.getElementById('notes_table_body'),
 		game_version = document.getElementById('game_version_new').checked ? 'white' : 'orchid',
 		suspects = all_suspects;
-	console.log(suspects);
 	delete suspects[game_version];
-	console.log(suspects);
 	Object.entries(suspects).forEach((suspect) => {
 		let [key, value] = suspect;
 		const row_group = document.createElement('tr');
@@ -81,6 +100,7 @@ function add_separator_row(label) {
 	const row = document.createElement('tr'),
 		row_label = document.createElement('th'),
 		table_body = document.getElementById('notes_table_body');
+	row_label.classList.add('divider');
 	row_label.innerHTML = label;
 	row.append(row_label);
 	table_body.append(row);
@@ -122,5 +142,25 @@ function add_event_listeners_to_buttons() {
 	const buttons = document.getElementsByClassName('change_icon');
 	for (let $i = 0; $i < buttons.length; $i++) {
 		buttons[$i].addEventListener('click', update_icon);
+	}
+}
+
+function set_player_name_height(player_count) {
+	let largest_width = 0;
+	for (let $i = 0; $i <= player_count; $i++) {
+		if ($i > 0) {
+			const span = document.getElementById(`player_name_${$i}`);
+			const span_height = span.getBoundingClientRect().height;
+			console.log({ span_height });
+			largest_width = largest_width < span_height ? span_height : largest_width;
+		}
+	}
+	const headings = document.getElementsByClassName('player_heading');
+	for (let $i = 0; $i < headings.length; $i++) {
+		if (largest_width < 32) {
+			headings[$i].style.height = `3rem`;
+		} else {
+			headings[$i].style.height = `calc(${largest_width}px + 1rem)`;
+		}
 	}
 }
