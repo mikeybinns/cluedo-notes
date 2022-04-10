dc6ca4e139f10d025cbf88520c298398b6fba479
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2022-04-10 21:35:02 +0100

    revert: revert all changes made today
    Added commits made the project messy and full of config files
    The original code was much cleaner.
    I intend to reintroduce all the config in a scripts package.

--- js/create_start_form.js
+++ js/create_start_form.js
@@ -1,45 +1,45 @@
-import { suspects, weapons, locations } from "./data.js";
+import { suspects, weapons, locations } from './data.js';
 export function create_suspect_select() {
-	const select = document.createElement("select");
+	const select = document.createElement('select');
 	const suspects_array = Object.entries(suspects);
 	suspects_array.forEach((suspect) => {
 		let [key, value] = suspect;
-		let option = document.createElement("option");
-		option.innerHTML = value["long name"];
+		let option = document.createElement('option');
+		option.innerHTML = value['long name'];
 		select.append(option);
 	});
 	return select;
 }
 export function create_weapon_select() {
-	const select = document.createElement("select");
+	const select = document.createElement('select');
 	weapons.forEach((weapon) => {
-		let option = document.createElement("option");
+		let option = document.createElement('option');
 		option.innerHTML = weapon.name;
 		select.append(option);
 	});
 	return select;
 }
 export function create_room_select() {
-	const select = document.createElement("select");
+	const select = document.createElement('select');
 	locations.forEach((room) => {
-		let option = document.createElement("option");
+		let option = document.createElement('option');
 		option.innerHTML = room.name;
 		select.append(option);
 	});
 	return select;
 }
 export function update_player_fields() {
-	const player_count = document.getElementById("player_count").value;
+	const player_count = document.getElementById('player_count').value;
 	for (let $i = 0; $i < 6; $i++) {
 		const player_number = $i + 1,
 			field_group = document.getElementById(`field_player_${player_number}`),
 			input = document.getElementById(`player_${player_number}`);
 		if (player_number <= player_count) {
 			input.required = true;
-			field_group.classList.remove("hide");
+			field_group.classList.remove('hide');
 		} else {
 			input.required = false;
-			field_group.classList.add("hide");
+			field_group.classList.add('hide');
 		}
 	}
 }
--- sw.js
+++ sw.js
@@ -1,39 +1,39 @@
-var APP_PREFIX = "cluedo_notes_"; // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = "version_1.0.1"; // Version of the off-line cache (change this value everytime you want to update cache)
+var APP_PREFIX = 'cluedo_notes_'; // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_1.0.1'; // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION;
 var URLS = [
-	"/cluedo-notes/",
-	"/cluedo-notes/index.html",
-	"/cluedo-notes/fonts/Futura-CondensedBoldOblique.otf",
-	"/cluedo-notes/fonts/Futura-CondensedExtraBold.otf",
-	"/cluedo-notes/fonts/Futura-CondensedExtraBold.woff",
-	"/cluedo-notes/fonts/Futura-CondensedExtraBold.woff2",
-	"/cluedo-notes/img/cluedo.jpg",
-	"/cluedo-notes/img/dr-orchid.jpg",
-	"/cluedo-notes/img/mrs-white.jpg",
-	"/cluedo-notes/js/create_start_form.js",
-	"/cluedo-notes/js/create_table.js",
-	"/cluedo-notes/js/data.js",
-	"/cluedo-notes/js/update_icon.js",
-	"/cluedo-notes/styles.css",
-	"/cluedo-notes/icons/favicon.png",
-	"/cluedo-notes/icons/logo192.png",
-	"/cluedo-notes/icons/logo512.png",
-	"/cluedo-notes/manifest.json",
+	'/cluedo-notes/',
+	'/cluedo-notes/index.html',
+	'/cluedo-notes/fonts/Futura-CondensedBoldOblique.otf',
+	'/cluedo-notes/fonts/Futura-CondensedExtraBold.otf',
+	'/cluedo-notes/fonts/Futura-CondensedExtraBold.woff',
+	'/cluedo-notes/fonts/Futura-CondensedExtraBold.woff2',
+	'/cluedo-notes/img/cluedo.jpg',
+	'/cluedo-notes/img/dr-orchid.jpg',
+	'/cluedo-notes/img/mrs-white.jpg',
+	'/cluedo-notes/js/create_start_form.js',
+	'/cluedo-notes/js/create_table.js',
+	'/cluedo-notes/js/data.js',
+	'/cluedo-notes/js/update_icon.js',
+	'/cluedo-notes/styles.css',
+	'/cluedo-notes/icons/favicon.png',
+	'/cluedo-notes/icons/logo192.png',
+	'/cluedo-notes/icons/logo512.png',
+	'/cluedo-notes/manifest.json',
 ];
 
 // Respond with cached resources
-self.addEventListener("fetch", function (e) {
-	console.log("fetch request : " + e.request.url);
+self.addEventListener('fetch', function (e) {
+	console.log('fetch request : ' + e.request.url);
 	e.respondWith(
 		caches.match(e.request).then(function (request) {
 			if (request) {
 				// if cache is available, respond with cache
-				console.log("responding with cache : " + e.request.url);
+				console.log('responding with cache : ' + e.request.url);
 				return request;
 			} else {
 				// if there are no cache, try fetching request
-				console.log("file is not cached, fetching : " + e.request.url);
+				console.log('file is not cached, fetching : ' + e.request.url);
 				return fetch(e.request);
 			}
 
@@ -44,17 +44,17 @@ self.addEventListener("fetch", function (e) {
 });
 
 // Cache resources
-self.addEventListener("install", function (e) {
+self.addEventListener('install', function (e) {
 	e.waitUntil(
 		caches.open(CACHE_NAME).then(function (cache) {
-			console.log("installing cache : " + CACHE_NAME);
+			console.log('installing cache : ' + CACHE_NAME);
 			return cache.addAll(URLS);
 		})
 	);
 });
 
 // Delete outdated caches
-self.addEventListener("activate", function (e) {
+self.addEventListener('activate', function (e) {
 	e.waitUntil(
 		caches.keys().then(function (keyList) {
 			// `keyList` contains all cache names under your username.github.io
@@ -68,7 +68,7 @@ self.addEventListener("activate", function (e) {
 			return Promise.all(
 				keyList.map(function (key, i) {
 					if (cacheWhitelist.indexOf(key) === -1) {
-						console.log("deleting cache : " + keyList[i]);
+						console.log('deleting cache : ' + keyList[i]);
 						return caches.delete(keyList[i]);
 					}
 				})
